0a9004402d72d42366d7721e70cbb90fcfd2f907
Camilo <ortizdiazcamiloandres@gmail.com>
2025-09-25 10:17:29 -0500

    Subiendo el archivo inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+""" Integrantes:
+    * Han's De Ávila
+    * Andrés Narvaez
+    * Isabel Lara
+    * Sebastian Mejía
+    * Camilo Ortíz 
+"""
+
+def sumarNumeros (n1,n2):
+    suma=n1+n2
+    print(suma)
+
+def restarNumeros (n1,n2):
+    resta=n1-n2
+    print(resta)
+
+sumarNumeros(1,2)
+restarNumeros(3,5)
