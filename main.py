2aeb8b6383b66e23969478a0bb8a6311702b5870
Han's de Ávila <144625014+srhanscho@users.noreply.github.com>
2025-09-25 10:33:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,27 @@
     * Camilo Ortíz 
 """
 
-def sumarNumeros (n1,n2):
-    suma=n1+n2
-    print(suma)
+def sumarNumeros(n1, n2):
+    suma = n1 + n2
+    print("Suma:", suma)
 
-def restarNumeros (n1,n2):
-    resta=n1-n2
-    print(resta)
+def restarNumeros(n1, n2):
+    resta = n1 - n2
+    print("Resta:", resta)
+
+def multiplicarNumeros(n1, n2):
+    multiplicacion = n1 * n2
+    print("Multiplicación:", multiplicacion)
+
+def dividirNumeros(n1, n2):
+    if n2 != 0:
+        division = n1 / n2
+        print("División:", division)
+    else:
+        print("Error: No se puede dividir entre 0")
 
 if __name__ == "__main__":
-    sumarNumeros(1,2)
-    restarNumeros(3,5)
+    sumarNumeros(1, 2)
+    restarNumeros(3, 5)
+    multiplicarNumeros(4, 6)
+    dividirNumeros(10, 2)
